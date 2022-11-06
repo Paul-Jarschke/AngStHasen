4b09e0e34014a005b4e680e6184d3526bbcd25e6
prichi99 <77013132+prichi99@users.noreply.github.com>
2022-11-07 00:32:38 +0100

    Task 1,2

---
+++ assignment/01_Paul_Jarschke/assignment_01/task_02.py
@@ -0,0 +1,22 @@
+# Task 02
+# Write a program that reads in a word provided by the user and prints the word in a reversed order.
+# For example, if the input is hello, the output should be ‘olleh’.
+
+# a. Use a loop structure
+print('Which word would you like to reverse')
+word_a = input()
+
+reverse_a = ''
+for i in word_a:
+    reverse_a = i + reverse_a
+
+print('The reversed order of your word is : ' + reverse_a)
+
+
+# b. Use just array indexes
+print('Which word would you like to reverse?')
+word_b = input()
+
+reverse_b = word_b[::-1]
+
+print('The reversed order of your word is : ' + reverse_b)
