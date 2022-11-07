5203a1c1af32299811bc15444e6dbd26c54de5be
soenkehaenel <117224374+soenkehaenel@users.noreply.github.com>
2022-11-07 19:05:11 +0100

    Update task 06.py
    Updated to include the a. part of the task. Note that if the user enters an integer the output will still feature a floating point. This could easily be circumwented by a branch, but would entail even more loss in the readability of le programmé.

--- assignment/02_Sönke_Hänel/assignment_01/task 06.py
+++ assignment/02_Sönke_Hänel/assignment_01/task 06.py
@@ -22,3 +22,50 @@ while input_integer != 0:
 
 # output
 print(octal_string)
+
+
+# a. modification
+
+# user input. make sure to only accept non-negative integers.
+input_float = -1.0
+while input_float < 0:
+    input_float = float(input("Provide a non-negative floating point number in the decimal representation please.\n"))
+
+# the idea is to just cut the float into two pieces: the integer part and the part behind the decimal point
+
+# splitting of the two parts:
+integer_part = int(input_float)
+input_float -= integer_part
+
+# treatment of part before decimal point:
+
+# initialize empty string
+octal_string = ""
+# As long as the number is not 0,
+# remainder of the division of the number by 8 is the next (bigger) octal potency.
+# the number without remainder by 8 and continue with the next octal digit.
+while integer_part != 0:
+    octal_string = str(integer_part % 8) + octal_string
+    integer_part = integer_part // 8
+
+# add "octal"point
+octal_string = octal_string + "."
+
+# treatment of part after decimal point
+# we can run into the problem that we have an eternally repeating sequence of numbers,
+# so we specify a maximum of 16 octal places. i is counting these
+i = 0
+
+# in each loop multiply the floating point part by 8 and
+# take the integer part of the result as the next octal of the number
+
+while input_float != 0.0 and i <= 15:
+    input_float *= 8
+    integer_part = int(input_float)
+    input_float -= integer_part
+    octal_string += str(integer_part % 8)
+    integer_part = integer_part // 8
+    i += 1
+
+# output
+print(octal_string)
