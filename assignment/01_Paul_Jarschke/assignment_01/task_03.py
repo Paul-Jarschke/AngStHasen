891dd284fd7e440cc88899d82c5d1a2ce37a956d
prichi99 <77013132+prichi99@users.noreply.github.com>
2022-11-08 19:31:29 +0100

    Task 3

--- assignment/01_Paul_Jarschke/assignment_01/task_03.py
+++ assignment/01_Paul_Jarschke/assignment_01/task_03.py
@@ -1,7 +1,7 @@
 # Task 03
 
-# Write a program that reads in an upper bound (number) provided by the user and prints the sequence of Fibonacci numbers that are less or equal to the number given by the user.
-# Use a while-loop for this task.
+# Write a program that reads in an upper bound (number) provided by the user and prints the sequence of Fibonacci
+# numbers that are less or equal to the number given by the user. Use a while-loop for this task.
 
 print('Please provide and upper bound (number) for the Fibonacci sequence.')
 upper_bound = int(input())
