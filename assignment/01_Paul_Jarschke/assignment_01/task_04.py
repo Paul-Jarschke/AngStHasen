702ecfe7543b6394020c979a44b83343b6b435dc
prichi99 <77013132+prichi99@users.noreply.github.com>
2022-11-08 19:22:17 +0100

    Task 4

--- assignment/01_Paul_Jarschke/assignment_01/task_04.py
+++ assignment/01_Paul_Jarschke/assignment_01/task_04.py
@@ -1,10 +1,11 @@
 # Task 4
 
-# Write a program that reads in an upper bound (number) provided by the user and prints all integer numbers that are less or equal to the upper bound
-# except the integer numbers that are divisible by 3.
+# Write a program that reads in an upper bound (number) provided by the user and prints all integer numbers that are
+# less or equal to the upper bound except the integer numbers that are divisible by 3.
 # Use the continue statement. For example, if the user enters 6 as the upper bound, the output should be 0, 1, 2, 4, 5.
 
-# Hint for Tutor: 0 is divisible by 3 and should not be p. In the given example 0 is printed out which is why I print 0 as well.
+# Hint for Tutor:
+# 0 is divisible by 3 and should not be p. In the given example 0 is printed out which is why I print 0 as well.
 
 print('Please provide and upper bound (number) for sequence.')
 upper_bound = int(input())
