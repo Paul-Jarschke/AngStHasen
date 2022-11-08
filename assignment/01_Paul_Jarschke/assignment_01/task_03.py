f4beda640906aaaa90c3038b3407f719e268da9f
prichi99 <77013132+prichi99@users.noreply.github.com>
2022-11-08 16:35:56 +0100

    Fibonacci solution

--- assignment/01_Paul_Jarschke/assignment_01/task_03.py
+++ assignment/01_Paul_Jarschke/assignment_01/task_03.py
@@ -6,21 +6,22 @@
 print('Please provide and upper bound for the Fibonacci sequence.')
 upper_bound = int(input())
 
-f_0 = 0
-f_1 = 1
-f_n = 1
+f_0 = 0  # First Number of Fibonacci Sequence
+f_1 = 1  # Second Number of Fibonacci Sequence
+f_n = 1  # Iteration Variable
 
-if upper_bound 0:
-    print('Fibonacci sequence is:'+ str(f_0) + ','+ str(f_1))
+if upper_bound == 0:
+    print(f'Your Fibonacci Sequence is:\n{f_0}')
 
-elif :
+elif upper_bound == 1:
+    print(f'Your Fibonacci Sequence is:\n{f_0}, {f_1}')
 
 else:
     print('Your Fibonacci Sequence is:')
-    print(str(f_0), end=', ')
+    print(f_0, end='')
 
     while f_n <= upper_bound:
         f_n = f_1 + f_0
         f_0 = f_1
         f_1 = f_n
-        print(f_0, end=', ')
+        print(f', {f_0}', end='')
