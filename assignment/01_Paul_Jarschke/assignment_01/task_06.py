fd100bb322f18b1849132f90872d373e64e967ab
prichi99 <77013132+prichi99@users.noreply.github.com>
2022-11-11 16:47:05 +0100

    Task as comment

---
+++ assignment/01_Paul_Jarschke/assignment_01/task_06.py
@@ -0,0 +1,7 @@
+# Task 6
+# Write a program that reads a non-negative integer number in the decimal representation provided by the user and prints
+# the octal representation of the number. For example, if the user enters 167, the output should be 247.
+
+# Write a more general program that reads in a non-negative number (potentially including decimal places) in the decimal
+# representation provided by the user and prints the octal representation of the number. For example, if the user enters
+# 25.11, the output should be 31.0702436560507534.
