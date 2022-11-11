bcb27a4596c929bce5cfbce0bbd85262b58e286d
prichi99 <77013132+prichi99@users.noreply.github.com>
2022-11-11 14:31:31 +0100

    Task 5

---
+++ assignment/01_Paul_Jarschke/assignment_01/task_05.py
@@ -0,0 +1,24 @@
+# Task 5
+# Write a Python program that asks the user to input the lengths of the sides in a triangle and outputs whether the
+# triangle is equilateral, isosceles, or scalene. The program should also check for the Triangle inequality (z < x + y),
+# and prompt the user for a valid input.
+
+print('Let\'s define the lengths of the sides for a triangle!')
+
+x, y, z = [float(x) for x in input('Please enter 3 numerical values and separate them by pressing space.\n').split()]
+lengths = [x, y, z]
+
+# Check for triangle inequality and reject input, until the input is valid
+while sum(lengths) <= 2*max(lengths):
+    print('Your input does not satisfy the condition for triangle inequality! Please enter a valid set of values.')
+    x, y, z = [float(x) for x in input().split()]
+    lengths = [x, y, z]
+
+print(f'Length of x: {x} (cm)\nLength of y: {y} (cm)\nLength of z: {z} (cm)\n')
+
+if x == y == z:
+    print('Your triangle is equilateral.')
+elif x != y != z:
+    print('Your triangle is scalene.')
+else:
+    print('Your triangle is isosceles.')
