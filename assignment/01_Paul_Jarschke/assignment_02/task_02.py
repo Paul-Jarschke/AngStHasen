5bc4bc53340f46e6f626bac166dcd4654f7306d4
prichi99 <77013132+prichi99@users.noreply.github.com>
2022-11-25 15:00:42 +0100

    Assignment 02: Task 01, 02

---
+++ assignment/01_Paul_Jarschke/assignment_02/task_02.py
@@ -0,0 +1,26 @@
+# Task 02
+# Write a program that generates a list of 10 random integers between 1 and 100 and then finds and prints the largest
+# element in the list. Do not use the built-in function max(). For example, if the input is [23,3,42,29,12,15,8,4,37,34]
+# , the output should be the largest element: 42.
+
+# import random module to generate random integers
+import random
+
+
+# function that creates a list of random integers (by default 10 integers between 1 and 100)
+def random_numbers(range_min=1, range_max=100, n=10, lst=None):
+    if lst is None:
+        lst = []
+    for _ in range(n):
+        num = random.randint(range_min, range_max)
+        lst.append(num)
+    return lst
+
+
+# call function and assign it
+nums = random_numbers()
+print(f'Your randomly generated list:\n{nums}')
+
+# sort in ascending order
+nums.sort()
+print(f'The largest value:\n{nums[-1]}')
