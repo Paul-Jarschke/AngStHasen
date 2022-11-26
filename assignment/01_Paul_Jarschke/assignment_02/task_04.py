27c03519e80e140c092e00ecebf631c784f91d19
prichi99 <77013132+prichi99@users.noreply.github.com>
2022-11-26 15:19:39 +0100

    Assignment_2_4: Implementation of key functions for sorting

--- assignment/01_Paul_Jarschke/assignment_02/task_04.py
+++ assignment/01_Paul_Jarschke/assignment_02/task_04.py
@@ -14,17 +14,44 @@
 import random
 
 
-# this code is still trash :/
-def tuple_generator(range_min=1, range_max=100, n_tuples=2, tuple_length=3, lst=[], lst2=[]):
-    for _ in range(n_tuples):
-        for _ in range(tuple_length):
-            num = random.randint(range_min, range_max)
-            print(num)
-            lst.append(num)
-            print(lst)
-            tup = tuple(lst)
-        lst2.append(tup)
-    return lst2
+def tuple_generator(range_min=1, range_max=100, n_tup=10, tuple_length=3, lst=None):
+    """Generating a list with n_tup tuples.
+       Each tuple holds tuple-length number of random integer between range_min and range_max. """
+    if lst is None:
+        lst = []
+    for _ in range(n_tup):
+        tup = tuple([random.randint(range_min, range_max) for _ in range(tuple_length)])
+        lst.append(tup)
+    return lst
 
 
-print(f'function call{tuple_generator()}')
+##############
+# Solution 1 #
+##############
+
+# generate
+list_of_tuples = tuple_generator()
+# using own function to access third value in
+sorted_by_third = sorted(list_of_tuples, key=lambda tup: tup[2])
+
+# print unsorted and sorted list
+print(f'Solution 1')
+print(f'List of tuples:\n{list_of_tuples}')
+print(f'List of tuples sorted:\n{sorted_by_third}')
+print('\n')
+
+##############
+# Solution 2 #
+##############
+from operator import itemgetter
+
+
+list_of_tuples = tuple_generator()
+# using itemgetter to access third values in each tuple
+# this approach is more efficient/faster, than using a lambda function
+sorted_by_third = sorted(list_of_tuples, key=itemgetter(2))
+
+# print unsorted and sorted list
+print(f'Solution 2')
+print(f'List of tuples:\n{list_of_tuples}')
+print(f'List of tuples sorted:\n{sorted_by_third}')
