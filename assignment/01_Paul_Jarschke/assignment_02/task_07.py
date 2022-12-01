d28b29de13886e52c813b070cfbacd7bbfbf2ae4
prichi99 <77013132+prichi99@users.noreply.github.com>
2022-12-01 17:30:09 +0100

    Assignment_2_7: added comments/ breaking conditions

--- assignment/01_Paul_Jarschke/assignment_02/task_07.py
+++ assignment/01_Paul_Jarschke/assignment_02/task_07.py
@@ -3,6 +3,12 @@
 # coming to the end of a queue. Whenever “next” is given as input, the program will print out the name on the turn.
 # The program finishes as soon as the queue is empty.
 
+# Since we start with an empty queue, we can not break the program before the user is able to enter something.
+# I chose to break the loop in two different scenarios:
+# 1) No Person was ever added to the queue and the first input is "next"
+# 2) The last person is removed from the list.
+
+# initiate empty list resembling the queue
 stack = []
 
 while True:
@@ -10,18 +16,23 @@ while True:
     expression = input('Please enter a name that should be added to the queue, or type "next".\n')
 
     if expression == 'next':
+
+        if not stack:
+            # stop if queue is empty
+            print('The queue is empty. Goodbye!')
+            break
         # pop item in front of queue
         turn = stack.pop(0)
         print(f'It is your turn {turn}!')
-        print(f'Remaining Queue: {stack}')
 
-        if stack == []:
-            # stop if queue
-            print('The queue is finally empty!')
+        if not stack:
+            # stop if queue is empty
+            print('The queue is empty. Goodbye!')
             break
+        else:
+            print(f'Remaining Queue: {stack}')
+
     else:
         # put name at the end of the stack
         stack.append(expression)
         print(f'Current Queue: {stack}')
-
-
