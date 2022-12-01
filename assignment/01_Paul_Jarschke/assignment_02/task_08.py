a1fcb90bf75468ee3ed95409a6145a98fb66dda6
prichi99 <77013132+prichi99@users.noreply.github.com>
2022-12-01 17:34:50 +0100

    Assignment_2_89: initial commit, task description

---
+++ assignment/01_Paul_Jarschke/assignment_02/task_08.py
@@ -0,0 +1,2 @@
+# Task 08 - Unlimited Power
+# Write a function with two arguments – x and n. The function returns the value of xn. Use recursion.
---
+++ assignment/01_Paul_Jarschke/assignment_02/task_09.py
@@ -0,0 +1,5 @@
+# Task 09 - Unlimited Power II
+# Using function for factorial and function xn from previous task, write a program that reads value of x and prints
+# approximate value of ex. Use this formula (Taylor series) for calculation.
+# To get precise value of ex, the series would have to be infinite. Suppose that there is some required accuracy, so the
+# calculation finishes as soon as the value of the next element is smaller than given threshold (e.g., 0.000001).
