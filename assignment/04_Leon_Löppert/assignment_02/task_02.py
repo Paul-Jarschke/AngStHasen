b617da9fe3183fbbad23df60c6ff0c5a9abfa312
leon-l-247 <leon.loeppert@hotmail.de>
2022-12-04 18:30:25 +0100

    Task 2 finished

---
+++ assignment/04_Leon_Löppert/assignment_02/task_02.py
@@ -0,0 +1,30 @@
+################################################
+#### Programing for Data Scientists: Python ####
+####              Assignment 02             ####
+####             by Leon Löppert            ####
+################################################
+import random
+
+# Task 02 – Largest List Element ----
+
+# Write a program that generates a list of 10 random integers between 1 and 100 and then finds and prints the
+# largest element in the list. Do not use the built-in function max(). For example, if the input is
+# [23,3,42,29,12,15,8,4,37,34], the output should be the largest element: 42.
+
+import random
+random_ints = []
+i = 0
+
+
+while i <= 9:
+    random_ints.append(random.randrange(1, 100))
+    i += 1
+print("My list of random integers is:\n" + str(random_ints))
+
+maximum = 0
+for index, int in enumerate(random_ints):
+    if random_ints[index] > maximum:
+        maximum = random_ints[index]
+
+print("The largest number of this list is:\n" + str(maximum))
+
