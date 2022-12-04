3a6f7f50bf46d24e0be9c89d77fdd7903bde1f28
leon-l-247 <leon.loeppert@hotmail.de>
2022-12-04 18:11:35 +0100

    Task 1 finished

--- assignment/04_Leon_Löppert/assignment_02/task_01.py
+++ assignment/04_Leon_Löppert/assignment_02/task_01.py
@@ -9,10 +9,10 @@
 # Write a program that reads in a string and prints the length of the input string. Do not use any built-in functions
 # of Python, such as len(). For example, if the input is “Computer Science”, the output should be length: 16.
 
-word = input("Type something and I count the length of it:\n")
+word = str(input("Type something and I count the length of it:\n"))
 i = 0
 
 for letter in word:
     i = i + 1
 
-print("The length of your string is " + str(i) + ".")
+print("The length of your input is " + str(i) + ".")
