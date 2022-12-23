30b12ee0f7418cd41a8b66040f5e970d459e6104
prichi99 <77013132+prichi99@users.noreply.github.com>
2022-12-23 14:47:23 +0100

    Assignment_4_2 : Input

---
+++ assignment/01_Paul_Jarschke/assignment_04/task_02.py
@@ -0,0 +1,14 @@
+# Task 2 - List Comprehensions
+
+# Python supports list comprehension. The basic syntax of list comprehensions is:
+# L = [<elem> for <elem> <Condition>]
+# Use list comprehensions to write a Python function remove_long_words() that:
+#
+#   -  accepts a sentence s and an integer n as input parameters
+#   -  uses the split() function of String objects to split the sentence into words
+#   -  stores the individual words in a list
+#   -  removes all words that are longer than n characters from the list, thereby creating a new list
+#   -  prints the list to stdout
+
+s = input('Please enter a sentence: ')
+print(s)
