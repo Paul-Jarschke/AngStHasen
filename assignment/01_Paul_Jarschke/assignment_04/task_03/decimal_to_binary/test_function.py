d55f3ec4efd46f1f9eb08cbd8a1ddf0f9291af76
prichi99 <77013132+prichi99@users.noreply.github.com>
2022-12-26 12:49:25 +0100

    Assignment_4_3 : Unit test wroking :) (implementing relative imports)

--- assignment/01_Paul_Jarschke/assignment_04/task_03/decimal_to_binary/test_function.py
+++ assignment/01_Paul_Jarschke/assignment_04/task_03/decimal_to_binary/test_function.py
@@ -4,11 +4,11 @@ import os
 # set parent directory
 sys.path.append(os.path.join(os.path.dirname(__file__), os.path.pardir))
 
-from function import decimal2binary
+from . import function
 
 
 class Decimal2BinaryTestCase(unittest.TestCase):
     """Test for function.py (includes decimal2binary function)"""
     def test_negative_one_to_binary(self):
-        result = decimal2binary(-1)
-        self.assertTrue(-1)
+        result = function.decimal2binary(-1)
+        self.assertTrue(-1, result)
--- assignment/01_Paul_Jarschke/assignment_04/task_03/decimal_to_binary/test_function_correct.py
+++ assignment/01_Paul_Jarschke/assignment_04/task_03/decimal_to_binary/test_function_correct.py
@@ -2,13 +2,13 @@ import unittest
 import sys
 import os
 # set parent directory
-#sys.path.append(os.path.join(os.path.dirname(__file__), os.path.pardir))
+# sys.path.append(os.path.join(os.path.dirname(__file__), os.path.pardir))
 
-from function_correct import decimal_to_binary_correct
+from . import function_correct
 
 
 class Decimal2BinaryTestCase(unittest.TestCase):
     """Test for function.py (includes decimal2binary function)"""
     def test_negative_one_to_binary(self):
-        result = decimal_to_binary_correct(1)
+        result = function_correct.decimal_to_binary_correct(1)
         self.assertEqual(result, 1)
