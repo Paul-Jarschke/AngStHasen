09bfaec1ce812e2b5b8af826e69b9a530438bc45
prichi99 <77013132+prichi99@users.noreply.github.com>
2022-12-27 00:27:22 +0100

    Assignment_4_4 : ContentTooShortRemoved, Url example for each error

--- assignment/01_Paul_Jarschke/assignment_04/task_04.py
+++ assignment/01_Paul_Jarschke/assignment_04/task_04.py
@@ -7,9 +7,7 @@
 # - handles the exceptions thrown by the urllib.request function
 
 from urllib.request import urlopen
-from urllib.error import HTTPError
-from urllib.error import URLError
-from urllib.error import ContentTooShortError
+from urllib.error import HTTPError, URLError
 
 
 def open_url(url):
@@ -20,24 +18,28 @@ def open_url(url):
         return doc
     except HTTPError as http_e:
         print(f'{http_e} while connecting to : {url}')
-        print("%s while connecting to: %s" % (http_e, url))
         return None
     except URLError as url_e:
         print(f'{url_e} while connecting to : {url}')
-        print("%s while connecting to: %s" % (url_e, url))
-    except ContentTooShortError as content_e:
-        print(f'{content_e} while connecting to : {url}')
-        print("%s while connecting to: %s" % (content_e, url))
         return None
 
 
 print("\nHTTP Connection:")
-print("===========================")
+print("_______________________________________________________________________________________________________________")
 
-url = "http://www.umsl.edu/~siegelj/newcourse/part1/URL-HTTP.html"
+# Urls for testing
+# working url
+# 'https://docs.python.org/3/library/urllib.error.html#urllib.error.ContentTooShortError'
+# raise http error (404)
+# 'https://www.uni-goettingen.de/de/hilfskraft+php/sql+gesucht+%2810+std./monat%29/667669.html'
+# raise url error
+# 'http://www.iamnotarealaddress.de'
+
+url = 'https://docs.python.org/3/library/urllib.error.html#urllib.error.ContentTooShortError'
 print("Trying to connect to: " + url)
 html = open_url(url)
 
 # printing the whole html script
+# use print(html) to get full information
 if html is not None:
-    print(html)
+    print(html[0:200])
