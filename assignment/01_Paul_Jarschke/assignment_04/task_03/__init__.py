88899762ebc3069ccb78ff24eb7a9986e08f023e
prichi99 <77013132+prichi99@users.noreply.github.com>
2022-12-26 16:42:14 +0100

    Assignment_4_3 : Comment task description

---
+++ assignment/01_Paul_Jarschke/assignment_04/task_03/__init__.py
@@ -0,0 +1,5 @@
+# Task 3 - Unit Tests
+
+# The following algorithm in Python converts numbers in decimal representation to binary.
+# Develop a unit test that checks for values in the interval [-1,3] whether the algorithm returns the expected results.
+# Adjust the algorithm, so it passes the unit test developed in 1). Rename the function to decimal_to_binary_correct()
