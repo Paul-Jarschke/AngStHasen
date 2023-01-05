871c154581288f3c4d4cfa4ea1ff330aba8c3ec3
leon-l-247 <leon.loeppert@hotmail.de>
2023-01-05 15:01:20 +0100

    A4-T3: first approaches

---
+++ assignment/04_Leon_Löppert/assignment_04/task_03/decimal2binary_incorrect.py
@@ -0,0 +1,10 @@
+import math
+
+def decimal2binary(n):
+    # function to convert decimal integers to binary
+    x = []
+    while n > 0:
+        x.append(n % 2)
+        n = math.floor(n/2)
+    return x[::-1]
+
---
+++ assignment/04_Leon_Löppert/assignment_04/task_03/task_03.py
@@ -0,0 +1,23 @@
+#### Programing for Data Scientists: Python ####
+####              Assignment 04             ####
+####             by Leon Löppert            ####
+################################################
+
+# Task 03 - Unit Test ----
+
+# The algorithm in decimal2binary_incorrect.py converts numbers in decimal representation to binary.
+#
+# Develop a unit test that checks for values in the interval [-1,3] whether the algorithm returns the expected results.
+# Adjust the algorithm, so it passes the unit test developed in 1). Rename the function to
+# decimal_to_binary_correct()
+
+
+# Correct binary numbers are retrieved from:
+# https://www.rapidtables.com/convert/number/decimal-to-binary.html
+
+
+# Overview:
+# - decimal2binary_incorrect.py: contains the possibly wrong function which should be tested
+# - unittest.py: contains unit tests for the function in the interval [-1,3]
+# - decimal2binary_correct.py: contains a corrected version of the function
+
