752a83562db8692eaa1d758e263ed1779ef3d567
leon-l-247 <leon.loeppert@hotmail.de>
2023-01-06 01:46:18 +0100

    Task 03 finished.

---
+++ assignment/04_Leon_Löppert/assignment_04/task_03/decimal2binary_correct.py
@@ -0,0 +1,19 @@
+import math
+
+def decimal_to_binary_correct(n):
+    # function to convert decimal integers to binary
+    x = []
+    sign = 1
+    if n < 0:
+        sign = -1
+        n *= -1
+    elif n == 0:
+        x.append(0)
+    while n > 0:
+        x.append(n % 2)
+        n = math.floor(n/2)
+    binary = x[::-1]
+    return int(''.join([str(d) for d in binary]))*sign
+
+
+# negatives Ding noch einbauen. Brauch man das Join dann überhaupt?
