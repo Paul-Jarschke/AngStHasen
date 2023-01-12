8ce359c69b6323b5c2cd2fb773dfc8d6aaa461e7
leon-l-247 <leon.loeppert@hotmail.de>
2023-01-12 23:03:21 +0100

    Help button and provisionally help page implemented.

--- project/seats/flightseats/url.py
+++ project/seats/flightseats/url.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name = 'flightseats-home'),
-    path('booking/<int:flightnumber>',views.booking, name = 'booking'),
-    path('login/', views.login, name = 'login')
+    path('', views.home, name='flightseats-home'),
+    path('booking/<int:flightnumber>', views.booking, name='booking'),
+    path('login/', views.login, name='login', ),
+    path('help/', views.help, name='help')
 ]
--- project/seats/flightseats/views.py
+++ project/seats/flightseats/views.py
@@ -4,8 +4,8 @@ from .models import Flight, Seats
 flights = [
     {
         'year': '2015',
-        'month' : '1',
-        'day' : '1',
+        'month': '1',
+        'day': '1',
         'airline': 'US',
         'flight_number': '112',
         'origin_airport': 'SEA',
@@ -15,36 +15,37 @@ flights = [
     },
     {
         'year': '2015',
-        'month' : '1',
-        'day' : '1',
+        'month': '1',
+        'day': '1',
         'airline': 'US',
-        'flight_number':'112',
+        'flight_number': '112',
         'origin_airport': 'SEA',
         'dest_airport': 'ANC',
         'departure': '23:00'
     }
 ]
 
+
 def home(request):
-    context ={
-        'flights':Flight.objects.all()
+    context = {
+        'flights': Flight.objects.all()
     }
-    return render(request, 'flightseats/home.html',context)
+    return render(request, 'flightseats/home.html', context)
+
 
 def booking(request, flightnumber):
-    context= {'flightnumber': flightnumber}
+    context = {'flightnumber': flightnumber}
     return render(request, 'flightseats/booking.html', context)
 
 
-
 def login(request):
-    context=  {
+    context = {
         'seats': Seats.objects.all()
     }
     return render(request, 'flightseats/login.html', context)
 
 
-
-
+def help(request):
+    return render(request, 'flightseats/help.html')
 
 # Create your views here.
