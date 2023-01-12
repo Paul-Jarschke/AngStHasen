a35c5ed62053d0a09f78eb3f0770010606dca1d9
prichi99 <77013132+prichi99@users.noreply.github.com>
2023-01-12 16:56:03 +0100

    Assignment_4_3 : substitute lists with np.array()

--- assignment/01_Paul_Jarschke/assignment_04/task_01.py
+++ assignment/01_Paul_Jarschke/assignment_04/task_01.py
@@ -4,10 +4,12 @@
 # Use a lambda function only to retain the even values in an array of integers. Test your function with an input array
 # of your choosing. Print the input array and the filtered output array to stdout.
 
-# input to list
-lst = list(map(int, input("Please enter an arbitrary number of integers and separate them by pressing space:\n").split()))
-print(f'Your list of integers:\n{lst}')
+import numpy as np
 
-# filter list for even values
-lst = list(filter(lambda x: (x % 2 == 0), lst))
-print(f'Filtered list with even integers:\n{lst}')
+# input to numpy.array
+array = np.array(list(map(int, input("Please enter a number of integers and separate them by pressing space:\n").split())))
+print(f'Your array of integers:\n{array}')
+
+# filter array for even values
+array = np.array(list(filter(lambda x: (x % 2 == 0), array)))
+print(f'Filtered array with even integers:\n{array}')
