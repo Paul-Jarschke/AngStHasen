a5118edf61dc52e380079578f11630ee300f0ec3
DerRiesenPanda <janparlesak25@gmail.com>
2023-01-16 00:08:01 +0100

    users3

--- project/seats/seats/settings.py
+++ project/seats/seats/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'flightseats.apps.FlightseatsConfig',
+    'users.app.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
