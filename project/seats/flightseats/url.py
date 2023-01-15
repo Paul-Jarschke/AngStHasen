a9129b9b2947c25d009bca396a3f5054114a04e2
leon-l-247 <leon.loeppert@hotmail.de>
2023-01-16 00:42:59 +0100

    Bug fixes

--- project/seats/flightseats/url.py
+++ project/seats/flightseats/url.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from users import views as views2
 
 urlpatterns = [
     path('', views.home, name='flightseats-home'),
     path('booking/<int:flightnumber>', views.booking, name='booking'),
     path('login/', views.login, name='login', ),
-    path('help/', views.help, name='help')
+    path('help/', views.help, name='help'),
+    path('register/', views2.register, name='register')
 ]
