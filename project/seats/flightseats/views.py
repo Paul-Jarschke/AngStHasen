98a573728c8a4e5c68ef52918f1536b4eba21657
leon-l-247 <leon.loeppert@hotmail.de>
2023-01-16 14:23:00 +0100

    Bug fixes.

--- project/seats/flightseats/views.py
+++ project/seats/flightseats/views.py
@@ -1,30 +1,6 @@
 from django.shortcuts import render
-from .models import Flight, Seats
+from .models import Flight, Seats, Book
 import numpy as np
-
-flights = [
-    {
-        'year': '2015',
-        'month': '1',
-        'day': '1',
-        'airline': 'US',
-        'flight_number': '112',
-        'origin_airport': 'SEA',
-        'dest_airport': 'ANC',
-        'departure': '23:00'
-
-    },
-    {
-        'year': '2015',
-        'month': '1',
-        'day': '1',
-        'airline': 'US',
-        'flight_number': '112',
-        'origin_airport': 'SEA',
-        'dest_airport': 'ANC',
-        'departure': '23:00'
-    }
-]
 
 
 def home(request):
@@ -39,23 +15,31 @@ def booking(request, flightnumber):
     return render(request, 'flightseats/booking.html', context)
 
 
-seat_data = np.loadtxt("flightseats/data/chartIn.txt", dtype='str')
-for i in range(len(seat_data)):
-    Seats.objects.create(
-        column_a=seat_data[i][0],
-        column_b=seat_data[i][1],
-        column_c=seat_data[i][2],
-        column_d=seat_data[i][3],
-        column_e=seat_data[i][4],
-        column_f=seat_data[i][5],
-        column_g=seat_data[i][6]
-    )
-
-
 def login(request):
+    if request.method == 'POST':
+        if request.POST.get('seat_choice'):
+            book = Book()
+            book.seat_choice = request.POST.get('seat_choice')
+            book.save()
+
     context = {
         'seats': Seats.objects.all()
     }
+    global seat_data
+    seat_data = np.loadtxt("flightseats/data/chartIn.txt", dtype='str')
+    entries = Seats.objects.all()
+    entries.delete()
+
+    for i in range(len(seat_data)):
+        Seats.objects.create(
+            column_a=seat_data[i][0],
+            column_b=seat_data[i][1],
+            column_c=seat_data[i][2],
+            column_d=seat_data[i][3],
+            column_e=seat_data[i][4],
+            column_f=seat_data[i][5],
+            column_g=seat_data[i][6]
+        )
     return render(request, 'flightseats/login.html', context)
 
 
