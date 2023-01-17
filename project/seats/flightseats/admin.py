0ab98bdef9805d08116b6223f351ab00ff894858
leon-l-247 <leon.loeppert@hotmail.de>
2023-01-17 21:09:42 +0100

    Upload seat row file and save as ChartIn.txt

--- project/seats/flightseats/admin.py
+++ project/seats/flightseats/admin.py
@@ -7,27 +7,37 @@ from django.core.files.storage import default_storage
 
 
 class TxtImportForm(forms.Form):
-    txt_upload= forms.FileField()
+    txt_upload = forms.FileField()
 
 
 class SeatAdmin(admin.ModelAdmin):
 
     def get_urls(self):
-        urls= super().get_urls()
-        new_urls= [path('txt-upload/', self.upload_txt)]
-        return new_urls+urls
+        urls = super().get_urls()
+        new_urls = [path('txt-upload/', self.upload_txt)]
+        return new_urls + urls
 
     def upload_txt(self, request):
         if request.method == "POST":
-            txt_file= request.FILES["txt_upload"]
+            txt_file = request.FILES["txt_upload"]
 
+            file_data = txt_file.read().decode('utf8')
+            txt_data = file_data.split("\n")
 
+            with open('flightseats/data/chartIn.txt', 'w') as f:
 
+                for i in txt_data:
+                    fields = i.split()
+                    fields = '\t'.join(fields)
+                    print(fields)
+                    f.write(fields)
+                    f.write('\n')
+            f.close()
 
         form = TxtImportForm()
-        data = {"form":form}
+        data = {"form": form}
         return render(request, "admin/txt_upload.html", data)
 
 
 admin.site.register(Book)
-admin.site.register(Flight,SeatAdmin)
+admin.site.register(Flight, SeatAdmin)
