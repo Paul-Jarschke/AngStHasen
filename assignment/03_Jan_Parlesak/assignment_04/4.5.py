532db741f223dbe14245a2cb13576f15d86c9c2a
DerRiesenPanda <janparlesak25@gmail.com>
2023-01-18 02:55:27 +0100

    txt_upload2

--- assignment/03_Jan_Parlesak/assignment_04/4.5.py
+++ assignment/03_Jan_Parlesak/assignment_04/4.5.py
@@ -3,18 +3,9 @@ import urllib
 import urllib.request
 from urllib.error import HTTPError,URLError
 
-url_python= 'https://en.wikipedia.org/wiki/Python_(programming_language)'
+url_python= 'https://en.wnotreallythoughedia.org/wiki/Python_(programming_language)'
 
-def init_log(file_name,file_mode,level,format,date_format):
-    logging.basicConfig(file_name=file_name,
-                        file_mode=file_mode,
-                        level=level,
-                        format=format,
-                        date_format=date_format)
-
-init_log(file_name='test.log', file_mode='a',level=logging.ERROR,format="%(asctime)s %(levelname)s %(message)s",date_format='%d-%m-%y %H:%M')
-
-logging.basicConfig(filename="log.log",filemode="a",format="%(asctime)s %(levelname)s %(message)s",date_format='%d-%m-%y %H:%M')
+logging.basicConfig(level=logging.INFO,filename="log.log",filemode="a",format="%(asctime)s - %(levelname)s - %(message)s")
 
 
 
@@ -27,9 +18,12 @@ def open_url(url):
         print(html_string[0:200])
     except urllib.error.HTTPError as e:
         logging.exception("Error")
+        return None
     except urllib.error.URLError as e:
         logging.exception("Error")
+        return None
     except urllib.error.ContentTooShortError() as e:
         logging.exception("Error")
 
-logging.shutdown()
+
+open_url(url_python)
