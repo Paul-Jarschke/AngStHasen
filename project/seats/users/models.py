b38216fc01a33baff7b49c2ec1a0dcce6156506b
Paul Jarschke <prj.nw@t-online.de>
2023-01-20 11:24:39 +0100

    Create user data for DB

--- project/seats/users/models.py
+++ project/seats/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Users(models.Model):
+    """User information for Database"""
+    name = models.CharField(max_length=100)
+    user_name = models.CharField(max_length=100)
+    password = models.CharField(max_length=50)
