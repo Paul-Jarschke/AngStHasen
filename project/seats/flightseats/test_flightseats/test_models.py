d09c1140cfbe31fc8a4c9fd30e4e82465a5a502b
Paul Richard Jarschke <prj.nw@t-online.de>
2023-01-23 19:42:49 +0100

    Model testing

--- project/seats/flightseats/test_flightseats/test_models.py
+++ project/seats/flightseats/test_flightseats/test_models.py
@@ -1,12 +1,25 @@
 from django.test import TestCase
-from ..models import Flight
+from ..models import Flight, Book, Seats, Seats2, User
 
 
 class TestModels(TestCase):
+    def test_Flight_str(self):
+        """Method '__str__' should be equal to airline title"""
+        flight = Flight.objects.create(airline='Angsthasen Flights')
+        self.assertEqual(str(flight), 'Angsthasen Flights')
 
-    def setUp(self):
-        pass
+    def test_Book_str(self):
+        """Method '__str__' should be equal to seat_choice title"""
+        book = Book.objects.create(seat_choice='A1')
+        self.assertEqual(str(book), 'A1')
 
-    def test_model_str(self):
-        airline = Flight.object.create(airline='Test Airline')
-    pass
+    def test_Seat_str(self):
+        """Method '__str__' should be equal to column_a"""
+        seats = Seats.objects.create(column_a='A')
+        self.assertEqual(str(seats), 'A')
+
+    def test_User_str(self):
+        """Method '__str__' should be equal to name"""
+        user = User.objects.create(name='Paul')
+        self.assertEqual(str(user), 'Paul')
+
