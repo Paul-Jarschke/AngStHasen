4e02f94d75ce62bdaac11da1e663e96451e88abe
Paul Richard Jarschke <prj.nw@t-online.de>
2023-01-24 23:16:15 +0100

    Test Register form

--- project/seats/users/test_users/test_forms.py
+++ project/seats/users/test_users/test_forms.py
@@ -1,1 +1,16 @@
 from django.test import TestCase
+from django.contrib.auth.forms import UserCreationForm
+from ..forms import UserRegisterForm
+
+
+class UserRegisterFormTest(TestCase):
+
+    def test_register_form(self):
+        data = {
+            'email': 'paul@test.de',
+            'username': 'Name',
+            'password1': 'test12345',
+            'password2': 'test12345'
+        }
+        form = UserRegisterForm(data)
+        self.assertTrue(form.is_valid())
