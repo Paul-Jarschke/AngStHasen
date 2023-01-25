8753eace6c7d7450fe63a78b66f597293c068459
Paul Richard Jarschke <prj.nw@t-online.de>
2023-01-25 17:00:15 +0100

    initial steps for testing views

--- project/seats/flightseats/test_flightseats/test_views.py
+++ project/seats/flightseats/test_flightseats/test_views.py
@@ -3,14 +3,29 @@ from django.urls import reverse
 from ..views import home, flights, booking, help
 
 
-class TestViews(TestCase):
+class TestViewsHome(TestCase):
 
-    def setUp(self) -> None:
-        client = Client()
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get('flightseats-home')
+        self.assertEqual(response.status_code, 200)
 
-    def test_home_GET(self):
-        response = self.client.get(reverse('list'))
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
 
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('/'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'flightseats/home.html')
+
+
+class TestViewsFlights(TestCase):
+
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get('flights')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/flights/')
+        self.assertEqual(response.status_code, 200)
 
