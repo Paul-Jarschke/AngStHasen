560c9c545a335871335c041895ebaff9cf7347b9
Paul Richard Jarschke <prj.nw@t-online.de>
2023-01-25 18:38:18 +0100

    url, template testing for views

--- project/seats/flightseats/test_flightseats/test_views.py
+++ project/seats/flightseats/test_flightseats/test_views.py
@@ -6,26 +6,36 @@ from ..views import home, flights, booking, help
 class TestViewsHome(TestCase):
 
     def test_view_url_accessible_by_name(self):
-        response = self.client.get('flightseats-home')
-        self.assertEqual(response.status_code, 200)
+        # get HttpRequest using name
+        response = self.client.get(reverse('flightseats-home'))
+        # check if HTTP request has been successfully completed
+        self.assertEqual(response.status_code, 200, 'HTTP request was unsuccessful!')
 
     def test_view_url_exists_at_desired_location(self):
+        # get HttpResponse using path
         response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
+        # check if HTTP request has been successfully completed
+        self.assertEqual(response.status_code, 200, 'HTTP request was unsuccessful!')
 
     def test_view_uses_correct_template(self):
-        response = self.client.get(reverse('/'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'flightseats/home.html')
+        response = self.client.get(reverse('flightseats-home'))
+        # get HttpResponse
+        self.assertEqual(response.status_code, 200, 'HTTP request was unsuccessful!')
+        self.assertTemplateUsed(response, 'flightseats/home.html', 'Usage of wrong html template')
 
 
 class TestViewsFlights(TestCase):
 
     def test_view_url_accessible_by_name(self):
-        response = self.client.get('flights')
-        self.assertEqual(response.status_code, 200)
+        response = self.client.get(reverse('flights'))
+
+        self.assertEqual(response.status_code, 200, 'HTTP request has been successfully completed')
 
     def test_view_url_exists_at_desired_location(self):
         response = self.client.get('/flights/')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 200, 'HTTP request has been successfully completed')
 
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('flights'))
+        self.assertEqual(response.status_code, 200, 'HTTP request has been successfully completed')
+        self.assertTemplateUsed(response, 'flightseats/flights.html', 'Usage of wrong html template')
