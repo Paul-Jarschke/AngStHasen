cfb180f2e9ce3a1cfec12f2033ba94e2711790af
Paul Richard Jarschke <prj.nw@t-online.de>
2023-01-25 02:00:06 +0100

    test views

--- project/seats/flightseats/test_flightseats/test_views.py
+++ project/seats/flightseats/test_flightseats/test_views.py
@@ -1,5 +1,16 @@
 from django.test import TestCase, Client
+from django.urls import reverse
+from ..views import home, flights, booking, help
 
 
 class TestViews(TestCase):
-    pass
+
+    def setUp(self) -> None:
+        client = Client()
+
+    def test_home_GET(self):
+        response = self.client.get(reverse('list'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'flightseats/home.html')
+
