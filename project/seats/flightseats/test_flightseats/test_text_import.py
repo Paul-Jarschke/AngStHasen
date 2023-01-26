555c4f6ebd299e0f583a082798514289a25d7240
prichi99 <77013132+prichi99@users.noreply.github.com>
2023-01-26 17:00:43 +0100

    test text import

--- project/seats/flightseats/test_flightseats/test_text_import.py
+++ project/seats/flightseats/test_flightseats/test_text_import.py
@@ -1,2 +1,11 @@
-from django.test import SimpleTestCase
+from django.test import TestCase
+import tempfile
+
+
+class TestReadFile(TestCase):
+
+    def test_file_reading(self):
+        with open('flightseats/data/chartIn.txt') as f:
+            content = f.readlines()
+        self.assertEqual(content, '12')
 
