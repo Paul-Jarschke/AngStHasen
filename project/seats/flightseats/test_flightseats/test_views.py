50cf5a7445d2e284ca4cc1fbaaabdd58a48c1247
prichi99 <77013132+prichi99@users.noreply.github.com>
2023-01-26 17:59:57 +0100

    testing booking

--- project/seats/flightseats/test_flightseats/test_views.py
+++ project/seats/flightseats/test_flightseats/test_views.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 
 class TestViewsHome(TestCase):
@@ -44,6 +45,37 @@ class TestViewsFlights(TestCase):
         self.assertTemplateUsed(response, 'flightseats/flights.html', 'Usage of wrong html template!')
 
 
+class TestViewsHelp(TestCase):
+
+    def test_view_url_accessible_by_name(self):
+        # Http request using name
+        response = self.client.get(reverse('help'))
+        # check for successful HTTP request
+        self.assertEqual(response.status_code, 200, 'HTTP request was unsuccessful!')
+
+    def test_view_url_exists_at_desired_location(self):
+        # Http request using path
+        response = self.client.get('/help/')
+        # check for successful HTTP request
+        self.assertEqual(response.status_code, 200, 'HTTP request was unsuccessful!')
+
+    def test_view_uses_correct_template(self):
+        # Http request using name
+        response = self.client.get(reverse('help'))
+        # check template usage
+        self.assertTemplateUsed(response, 'flightseats/help.html', 'Usage of wrong html template!')
+
+
+class BookingTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.client.login(username='testuser', password='12345')
+
+    def test_booking_view_POST(self):
+        response = self.client.post('/booking/', {'seat_choice_row': '1', 'seatletter': 'A'})
+        self.assertEqual(response.status_code, 200)
+
+
 class TestViewsBooking(TestCase):
 
     def test_view_url_accessible_by_name(self):
@@ -64,23 +96,3 @@ class TestViewsBooking(TestCase):
         # check template usage
         self.assertTemplateUsed(response, 'flightseats/booking.html', 'Usage of wrong html template!')
 
-
-class TestViewsHelp(TestCase):
-
-    def test_view_url_accessible_by_name(self):
-        # Http request using name
-        response = self.client.get(reverse('help'))
-        # check for successful HTTP request
-        self.assertEqual(response.status_code, 200, 'HTTP request was unsuccessful!')
-
-    def test_view_url_exists_at_desired_location(self):
-        # Http request using path
-        response = self.client.get('/help/')
-        # check for successful HTTP request
-        self.assertEqual(response.status_code, 200, 'HTTP request was unsuccessful!')
-
-    def test_view_uses_correct_template(self):
-        # Http request using name
-        response = self.client.get(reverse('help'))
-        # check template usage
-        self.assertTemplateUsed(response, 'flightseats/help.html', 'Usage of wrong html template!')
