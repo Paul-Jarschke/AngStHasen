639487e54aef9d814ee7d4e599c399c5ed805c0d
prichi99 <77013132+prichi99@users.noreply.github.com>
2023-01-26 11:28:25 +0100

    test register

--- project/seats/users/test_users/test_urls.py
+++ project/seats/users/test_users/test_urls.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
-from flightseats.views import home, flights, booking, help
+from ..views import register
+
+
+class TestUrls(TestCase):
+    def test_register_url_resolve(self):
+        url = reverse('register')
+        self.assertEqual(resolve(url).func, register)
+
